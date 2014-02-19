a7bb6637f873a48216f3a58da07bd6d9571d8270
DROESSMJ <droessmj@dplsubmit.cs.uwec.edu>
2014-02-19 15:31:50 -0600

    5

--- lab4/script5/script_05_weather.sh
+++ lab4/script5/script_05_weather.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+echo $0
+echo $1
+echo $2
+echo $3
+echo $4
+echo $5
+
 while getopts "f:" opt; do
 	case $opt in
 		f)
@@ -7,15 +14,15 @@ while getopts "f:" opt; do
 			#if there is an f flag, airport codes are all the args after
 			args=$3
 			nextArg=$3
-			$num=3
-			while [ "$nextArg" != "" ]
-				do
-				   if [ "$nextArg" != "$3" ]
-					then $args=$args$nextArg
-				   fi
-				   $num+=1
-				   nextArg="$"$num""
-			done
+			num=3
+#			while [ "$nextArg" != "" ]
+#				do
+#				   if [ "$nextArg" != "$3" ]
+#					then $args=$args$nextArg
+#				   fi
+#				   $num+=1
+#				   nextArg="$"$num""
+#			done
 		  ;;
 		\?)
 			echo "Invalid option: -$opt"
@@ -23,8 +30,8 @@ while getopts "f:" opt; do
 	esac
 done
 
-if ! grep -e "-f" 
-	then
+if ! grep -e "-f" $* 
+	then echo "no -f flag"
 fi
 
 #for location in $locations
