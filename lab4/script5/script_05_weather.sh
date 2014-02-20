5c09b2b1e6d553a66a1dd560d65bb08abda46d48
DROESSMJ <droessmj@dplsubmit.cs.uwec.edu>
2014-02-20 14:47:07 -0600

    finish 5

--- lab4/script5/script_05_weather.sh
+++ lab4/script5/script_05_weather.sh
@@ -1,45 +1,30 @@
 #!/bin/bash
 
-echo $0
-echo $1
-echo $2
-echo $3
-echo $4
-echo $5
-
 while getopts "f:" opt; do
 	case $opt in
 		f)
 			fields=$OPTARG
-			#if there is an f flag, airport codes are all the args after
-			args=$3
-			nextArg=$3
-			num=3
-#			while [ "$nextArg" != "" ]
-#				do
-#				   if [ "$nextArg" != "$3" ]
-#					then $args=$args$nextArg
-#				   fi
-#				   $num+=1
-#				   nextArg="$"$num""
-#			done
-		  ;;
+			shift $(( ${OPTIND} - 1 ));
+		  
+ 	 	  ;;
 		\?)
 			echo "Invalid option: -$opt"
 		  ;;
 	esac
 done
 
-if ! grep -e "-f" $* 
-	then echo "no -f flag"
-fi
 
-#for location in $locations
-#   do
-#	curl -s http://w1.weather.gov/xml/current_obs/$location.xml >> output
-#done
+for location in $*
+   do
+	curl -s http://w1.weather.gov/xml/current_obs/$location.xml >> weatherOutput
+	echo $location
+	IFS=',' read -a fieldArray <<< "$fields"
+	for field in "${fieldArray[@]}"
+	  do
+		grep -F "<$field>" weatherOutput >> output
+	done
+	cat output
+	rm weatherOutput
+	rm output
+done
 
-#parse output
-
-#cat output
-#rm output
