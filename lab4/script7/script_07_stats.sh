9b45c502f207099beb757d48b9d711b22d72d601
DROESSMJ <droessmj@dplsubmit.cs.uwec.edu>
2014-02-20 16:55:17 -0600

    started 7

---
+++ lab4/script7/script_07_stats.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+for arg in $*
+  do
+	if [ "$arg" = "sum" ]
+		then
+			
+	fi
+	
+	if [ "$arg" = "average" ]
+		then
+			
+	fi
+	
+	if [ "$arg" = "median" ]
+		then
+			
+	fi
+
+	if [ "$arg" = "mode" ]
+		then
+			
+	fi
+
+	if [ "$arg" = "min" ]
+		then
+			
+	fi
+
+	if [ "$arg" = "max" ]
+		then
+			
+	fi
+
+done
+
+
