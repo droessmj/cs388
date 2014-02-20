d83ce6c9dc9f647a173fe4be4d3f403ef933ac29
DROESSMJ <droessmj@dplsubmit.cs.uwec.edu>
2014-02-20 16:47:02 -0600

    finised script 6

--- lab4/script6/script_06_sort_by_frequency.sh
+++ lab4/script6/script_06_sort_by_frequency.sh
@@ -2,7 +2,7 @@
 
 cat > input
 
-while getopts ":rv:" opt; do
+while getopts ":rv" opt; do
 	case $opt in
 		r)
 		  	sortReverse=true
@@ -16,18 +16,23 @@ while getopts ":rv:" opt; do
 	esac
 done
 
-if [ "$sortReverse" = true && "frequency" = true ]
+
+
+
+if [ "$sortReverse" = true ] && [ "$frequency" = true ]
 	then 
-	   sort -r input > sorted
+	  sort -r input | uniq -c | awk '{print $2"\t"$1}' > sorted
 	else 
 		if [ "$sortReverse" = true ]
-			then sort -r input > sorted
-			else sort input > sorted
+			then sort -u -r input > sorted
+			else
+				if [ "$frequency" = true ]
+					then sort input | uniq -c | awk '{print $2"\t"$1}' > sorted
+					else sort -u input > sorted
+				fi
 		fi
 fi
 
-
-
 cat sorted 
 rm sorted
-
+rm input
