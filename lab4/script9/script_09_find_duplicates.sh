3e7b1927051ac8243de24983441ae6e1504c3f66
DROESSMJ <droessmj@dplsubmit.cs.uwec.edu>
2014-02-24 16:09:01 -0600

    polished off 9

--- lab4/script9/script_09_find_duplicates.sh
+++ lab4/script9/script_09_find_duplicates.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 touch hash_file
+
 if [[ -d $1 ]]
  then
 	for child in $1/*
- 	  if [[ -d $child ]] 
-		then echo $child is directory
-	  fi
-	  do
+	do
+ 	  	if [[ -d $child ]] 
+			then echo $child is directory
+	  	fi
+		
 		hash=$(md5sum $child)
+		
 		if grep $hash hash_file
-		  then rm $child 
+		  then echo $child
 		  else echo $hash >> hash_file
 		fi	
 	done  
